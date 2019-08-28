abff945b88d1baa4830693b4e489f9097249096d
freetimezp <kraftukrnet@ukr.net>
2019-08-28 21:48:22 +0300

    Добавление класса к компоненту

--- js/react.js
+++ js/react.js
@@ -1,6 +1,13 @@
 function Car(props) {
+
+	const classes = ['card']
+
+	if (props.car.marked) {
+		classes.push('marked')
+	}
+
 	return (
-		<div className="card">	
+		<div className={classes.join(' ')} onClick={props.onMark}>	
 			<div className="card-img">	
 				<img 
 					src={props.car.img} 
@@ -12,26 +19,39 @@ function Car(props) {
 		)
 }
 
+
 class App extends React.Component {
 
 	state = {
 		cars: [
-			{ name: 'BMW M2 Coupe', price: '20000', img: 'img/bmw-m2-1.jpg'},
-			{ name: 'Audi TT',	price: '15000',	img: 'img/audi-tt-1.jpg'},
-			{ name: 'Rolls Royce',	price: '50000',	img: 'img/rolls-royce-1.jpg'},
-			{ name: 'Mercedes amg',	price: '18000',	img: 'img/mercedes-amg-1.jpg'}
+			{ marked: false, name: 'BMW M2 Coupe', price: '20000', img: 'img/bmw-m2-1.jpg'},
+			{ marked: false, name: 'Audi TT',	price: '15000',	img: 'img/audi-tt-1.jpg'},
+			{ marked: false, name: 'Rolls Royce',	price: '50000',	img: 'img/rolls-royce-1.jpg'},
+			{ marked: false, name: 'Mercedes amg',	price: '18000',	img: 'img/mercedes-amg-1.jpg'}
  	]
  }
 
+	handleMarked(name) {
+		const cars = this.state.cars.concat()
 
- renderCars() {
- 	return this.state.cars.map(car => {
-				return ( <Car car={car} key={car.name + Math.random()} />	)
+		const car = cars.find(c => c.name === name)
+		car.marked = !car.marked
+
+		this.setState({ cars: cars })
+
+	}
+
+	renderCars() {
+		return this.state.cars.map(car => {
+			return ( 
+				<Car 
+					car={car} 
+					key={car.name + Math.random()} 
+					onMark={this.handleMarked.bind(this, car.name)}
+					/>	
+				)
 		}) 
- }
-
-
-
+	}
 
 	render() {
 		return (
