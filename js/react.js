a8a5b40607531e93ad4588cd67a859785c8de990
freetimezp <kraftukrnet@ukr.net>
2019-08-29 20:40:07 +0300

    Добавили кнопку и inputю Выводим на экран текст в инпуте. Пока интересно:)

--- js/react.js
+++ js/react.js
@@ -28,7 +28,9 @@ class App extends React.Component {
 			{ marked: false, name: 'Audi TT',	price: '15000',	img: 'img/audi-tt-1.jpg'},
 			{ marked: false, name: 'Rolls Royce',	price: '50000',	img: 'img/rolls-royce-1.jpg'},
 			{ marked: false, name: 'Mercedes amg',	price: '18000',	img: 'img/mercedes-amg-1.jpg'}
- 	]
+ 	],
+ 	visible: true,
+ 	appTitle: 'Cars application'
  }
 
 	handleMarked(name) {
@@ -42,6 +44,11 @@ class App extends React.Component {
 	}
 
 	renderCars() {
+
+		if (!this.state.visible) {
+			return null
+		}
+
 		return this.state.cars.map(car => {
 			return ( 
 				<Car 
@@ -53,9 +60,43 @@ class App extends React.Component {
 		}) 
 	}
 
+	toggleHandler() {
+		this.setState({visible: !this.state.visible})
+	}
+
+	titleChangeHandler(title) {
+		if (title.trim() === '') {
+			return
+		}
+
+		this.setState({
+			appTitle: title
+		})
+	}
+
+
+
 	render() {
+		const style = {
+			marginBottom: 20
+		}
+
 		return (
 			<div className="app">
+			 <h1>{this.state.appTitle}</h1>
+				<button 
+					onClick={() => this.toggleHandler()}
+					style={style}
+					>Some button</button>
+				<hr/>
+				<input 
+					type="text"
+					placeholder="Change title"
+					style={style}
+					onChange={(event) => this.titleChangeHandler(event.target.value)}
+					value={this.state.appTitle}
+					/>
+				<hr/>
 				<div className="list">
 					{ this.renderCars() }
 		 	</div>
